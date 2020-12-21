9074895d90056e70d5efe2d968b2ecccd7ac8582
Danila <Gdanila2002@yandex.ru>
2020-12-21 22:18:21 +0300

    задание 2

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ console.log(number+zero); // null приводится к числу
 console.log(string+bool); // булевая переменная приводится к строке
 console.log(string+zero); // null приводится к строке
 console.log(bool+zero); // и null, и булевая переменная приводятся к числам
+
+let a=10;
+let b=5;
+let s=a*b;
+let p=(a+b)*2;
+console.log(a+' '+b);
+console.log(p);
+console.log(s);
+console.log(p/s);
